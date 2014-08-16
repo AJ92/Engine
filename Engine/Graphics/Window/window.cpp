837b6b5eae2907c53fe4877b7c4be0a9965a0a2f
AJ92 <aj.3dev@gmail.com>
2014-08-17 01:21:13 +0200

    forward compatible mode disabled...

--- Engine/Graphics/Window/window.cpp
+++ Engine/Graphics/Window/window.cpp
@@ -68,7 +68,7 @@ void Window::initialize(){
 
     glfwWindowHint (GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint (GLFW_CONTEXT_VERSION_MINOR, 0);
-    glfwWindowHint (GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    //glfwWindowHint (GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint (GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 
